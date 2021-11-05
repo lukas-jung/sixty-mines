7de9f9bf8629c667ff7ad4a04814d0267f640e41
Lukas Jung <lukas.jung@mailbox.org>
2021-11-05 17:58:35 +0100

    Set data from rust

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
+use std::rc::Rc;
+
 sixtyfps::include_modules!();
 
 fn main() {
     let ui = AppWindow::new();
+    let minefield_model = Rc::new(sixtyfps::VecModel::<TileData>::from(vec![
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+        TileData {is_mine: false, opened: false, marked: false},
+    ]));
+    ui.set_minefield_model(sixtyfps::ModelHandle::new(minefield_model));
     ui.run();
 }
